ba46b50aa29a0c1f6f48d5da70fb656609596ae6
Tsurukage <77952357+Tsurukage@users.noreply.github.com>
2026-07-23 08:54:14 +0800

    from macbook
    From macbook version

---
+++ Assets/_Scripts/CarMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private float horizontal;
+    private float vertical;
+
+    [SerializeField]private float runSpeed;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        body.linearVelocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
